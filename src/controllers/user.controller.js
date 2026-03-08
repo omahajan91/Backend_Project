7ed53dc63a740eb4c33c0fae94760a142def9927
Om Mahajna <omrmahajan9477@gmail.com>
2026-03-08 13:48:24 +0530

    resolve some errors and the refresh and access Token problem

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,7 +11,7 @@ const generateAccessAndRefreshTokens = async(userId) => {
     const refreshToken = user.generateRefreshToken()
 
     user.refreshToken = refreshToken
-    user.save({ validateBeforeSave: false })
+    await user.save({ validateBeforeSave: false })
 
     return {accessToken, refreshToken}
 
@@ -87,7 +87,7 @@ const loginUser = asyncHandler(async (req,res) => {
    
   const {email, username, password} = req.body
 
-  if(!username || !email){
+  if(!username && !email){
     throw new ApiError(400, "username or email is required")
   }
 
